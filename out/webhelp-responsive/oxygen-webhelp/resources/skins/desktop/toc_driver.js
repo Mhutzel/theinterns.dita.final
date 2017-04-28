b842d7f574b7836d9e3d05f4275e16a9b58f4e68
esetaro <eliza96@vt.edu>
2017-04-28 17:00:17 -0400

    transform

--- out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170428045604", "oxygen-webhelp/search/index-1.js?uniqueId=20170428045604", "oxygen-webhelp/search/index-2.js?uniqueId=20170428045604", "oxygen-webhelp/search/index-3.js?uniqueId=20170428045604"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170428045915", "oxygen-webhelp/search/index-1.js?uniqueId=20170428045915", "oxygen-webhelp/search/index-2.js?uniqueId=20170428045915", "oxygen-webhelp/search/index-3.js?uniqueId=20170428045915"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
