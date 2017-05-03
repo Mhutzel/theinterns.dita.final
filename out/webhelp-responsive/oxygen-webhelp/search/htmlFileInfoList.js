0c15af6e682957dafe0f93597aba119f6bece7d0
beckyfelter <rfelter@vt.edu>
2017-05-02 21:58:06 -0400

    fixed the video

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,15 @@
+fil = new Array();
+fil["0"]= "c-benefits.html@@@Benefits of Internships@@@Want to learn more about the many benefits of internships?...";
+fil["1"]= "c-contact.html@@@Contact Information@@@Feel free to contact the Department of English with any general questions you may have or for more information you might need. 323 Shanks Hall 181 Turner Street NW Blacksburg, VA 24061 Phone...";
+fil["2"]= "c-credit.html@@@Internships for Credit@@@Want to receive college credit for an internship?...";
+fil["3"]= "c-eligibility.html@@@Eligibility@@@To learn more about internship eligibility click here...";
+fil["4"]= "c-intro.html@@@Welcome!@@@Remember the following when considering the internships you want to apply for: The internship should include a considerable amount of time writing in a professional setting A supervisor should be...";
+fil["5"]= "c-nocredit.html@@@Internships Without Credit@@@This guide is for all Virginia Tech students interested in obtaining an internship for compensation, rather than credit. This includes undergraduates (freshman through seniors) and graduate students of all majors seeking internship experience during college...";
+fil["6"]= "t-advisor.html@@@How to Consult your Advisor@@@If you feel as though you need outside help, you can meet with an advisor to help guide the way. Your advisor can also help you choose internships that best fit your needs...";
+fil["7"]= "t-apply.html@@@How to Apply@@@Applying for an internship can be a daunting task, but there are steps you can take to ensure that you are fully prepared...";
+fil["8"]= "t-compensation.html@@@How to Ensure Compensation@@@Many employers pay their interns. Every major has a different internship pay scale standard based on the field and experience level. In addition to wages, many employers add benefits to their internship compensation packets such as relocation assistance like a housing stipend...";
+fil["9"]= "t-credit.html@@@How to Apply for an Internship for College Credit@@@Apply for an internship to receive college credit...";
+fil["10"]= "t-cservices.html@@@How to Refer to Career Services@@@Career Services is an excellent resource to use to find internships that are perfect for you...";
+fil["11"]= "t-process.html@@@How to Begin@@@The first step to getting an internship is beginning the process. This page will show you exactly how to do so...";
+fil["12"]= "t-resources.html@@@Internship Resources@@@Want to discover different internship resources?...";
+var doStem = false;searchLoaded = true;
